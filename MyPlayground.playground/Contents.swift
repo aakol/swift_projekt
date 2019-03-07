2d67bed8641ad1dc636265e00ab74618ca97edc8
aakol <akol@poczta.onet.pl>
2019-03-07 09:40:27 -0800

    takie tam

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,56 @@
 import Cocoa
 
-var str = "Hello, playground"
+var str33 = "Hello, playground"
 var str2 = " ff"
-print(str, str2)
-print(str + str2)
+print(str33, str2)
+print(str33 + str2)
+
+var str = "Hello, playground"
+var sringles = "czesc swiecie"
+var asr = 34
+print(asr)
+var warosc = 38
+print(warosc)
+let a=895
+if a==89
+{
+    print("dddd")
+}
+else
+{
+    
+    print("ccc")
+}
+for a in 0...5
+{
+    print("uuu\(a)")
+}
+
+func funkcja(dane: String)-> String
+{
+    print("tekst \(dane) wypisane")
+    return dane
+}
+funkcja(dane: "osoba")
+func funkcja2(dane: String)-> Int
+{
+    print("tekst \(dane) wypisane")
+    return dane.count
+}
+print(funkcja2(dane: "wersjon t"))
+class Nowa_klaska
+{
+    var nameso: String = ""
+    func pierwsza(names: String) -> String {
+        nameso = names
+        return nameso
+    }
+}
+let opener = Nowa_klaska()
+print(opener.pierwsza(names: "wersja"))
+
+repeat {
+    print("czesc")
+    asr=asr-1
+} while (asr>0 && asr<45);
+
